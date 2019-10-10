f1b494b59020f9e30f5ccd1172d72bc2084e3410
Met at work <metssfm@gmail.com>
2019-10-10 18:56:23 +0200

    action setup influence fix I think?

--- mets_tools/setup_action_constraints.py
+++ mets_tools/setup_action_constraints.py
@@ -83,14 +83,19 @@ class SetupActionConstraints(bpy.types.Operator):
 
 			# Creating Action constraints
 			if(len(constraints)==0):
-				if(utils.flip_name(b.name)==b.name):	# Bone name unflippable, split constraint in two.
-					c_l = utils.find_or_create_constraint(b, 'ACTION', constraint_name + ".L")
-					constraints.append(c_l)
-					c_r = utils.find_or_create_constraint(b, 'ACTION', constraint_name + ".R")
-					constraints.append(c_r)
+				if(utils.flip_name(b.name)==b.name):
+					if ( utils.flip_name(self.subtarget) == self.subtarget ):
+						# If bone name and target name are unflippable, split constraint in two.
+						c_l = utils.find_or_create_constraint(b, 'ACTION', constraint_name + ".L")
+						c_l.influence=0.5
+						constraints.append(c_l)
+						c_r = utils.find_or_create_constraint(b, 'ACTION', constraint_name + ".R")
+						c_l.influence=0.5
+						constraints.append(c_r)
 				else:
 					c = utils.find_or_create_constraint(b, 'ACTION', constraint_name + suffix)
-				constraints.append(c)
+					c.influence=1
+					constraints.append(c)
 
 			# Configuring Action constraints
 			for c in constraints:
@@ -108,15 +113,12 @@ class SetupActionConstraints(bpy.types.Operator):
 					self.subtarget = self.subtarget[:-2]+".L"
 				if( c.name.endswith(".R") and self.subtarget.endswith(".L") ):
 					self.subtarget = self.subtarget[:-2]+".R"
-				
-				# If bone name cannot be flipped, it must be a center bone, so set influence to 0.5.
-				if( utils.flip_name(b.name)==b.name ):
-					c.influence=0.5
 
 				c.target_space = self.target_space
 				c.transform_channel = self.transform_channel
 				c.target = target
-				c.subtarget = self.subtarget
+				if self.subtarget != "":
+					c.subtarget = self.subtarget
 				c.action = action
 				c.min = self.trans_min
 				c.max = self.trans_max
