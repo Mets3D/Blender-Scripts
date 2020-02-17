88404cc460b3970a1b719e634dc82c5871cde3ca
Met at work <metssfm@gmail.com>
2020-02-17 17:17:30 +0100

    make save_copies a bit more universal.

--- save_copies.py
+++ save_copies.py
@@ -1,9 +1,33 @@
-# Save copies of this blend file under different names. Add extra renaming/processing/saving params as needed.
-
 import bpy, os
 
-copies = 8  # Excluding self.
+# Save copies of this blend file under different names. Add extra renaming/processing/saving params as needed.
+
+copies = 6  # Excluding self.
+
+def get_collections():
+	ret = []
+	for c in bpy.data.collections:
+		if c.name.startswith("CH-"):
+			ret.append(c)
+	return ret
+
+def get_rigs():
+	ret = []
+	for c in bpy.data.objects:
+		if c.name.startswith("RIG-"):
+			ret.append(c)
+	return ret
 
 for i in range(0, copies):
-    filepath = bpy.data.filepath.replace(".blend", ".%d.blend"%i)
-    bpy.ops.wm.save_as_mainfile(filepath=filepath, copy=True)
+	suffix = ".%d"%i
+	for c in get_collections():
+		if i > 0:
+			c.name = c.name[:-2]
+		c.name = c.name + suffix
+	for r in get_rigs():
+		if i > 0:
+			r.name = r.name[:-2]
+		r.name = r.name + suffix
+
+	filepath = bpy.data.filepath.replace(".blend", ".%d.blend"%i)
+	bpy.ops.wm.save_as_mainfile(filepath=filepath, copy=True)
