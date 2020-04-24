f48104d2e30bc4ecc94b652a3b102f02f2cecdd2
Mets3D <metssfm@gmail.com>
2020-04-24 19:53:30 +0200

    text

--- save_copies.py
+++ save_copies.py
@@ -2,32 +2,45 @@ import bpy, os
 
 # Save copies of this blend file under different names. Add extra renaming/processing/saving params as needed.
 
-copies = 6  # Excluding self.
+copies = 5  # Excluding self.
 
 def get_collections():
-	ret = []
-	for c in bpy.data.collections:
-		if c.name.startswith("CH-"):
-			ret.append(c)
-	return ret
+    ret = []
+    for c in bpy.data.collections:
+        if c.name.startswith("CH-") or c.name.startswith("PR-"):
+            ret.append(c)
+    return ret
 
 def get_rigs():
-	ret = []
-	for c in bpy.data.objects:
-		if c.name.startswith("RIG-"):
-			ret.append(c)
-	return ret
+    ret = []
+    for c in bpy.data.objects:
+        if c.name.startswith("RIG-"):
+            ret.append(c)
+    return ret
+
+def get_node_groups():
+    ret = []
+    for c in bpy.data.node_groups:
+        if c.name.startswith("VA-"):
+            ret.append(c)
+    return ret
 
 for i in range(0, copies):
-	suffix = ".%d"%i
-	for c in get_collections():
-		if i > 0:
-			c.name = c.name[:-2]
-		c.name = c.name + suffix
-	for r in get_rigs():
-		if i > 0:
-			r.name = r.name[:-2]
-		r.name = r.name + suffix
+    suffix = ".%d"%i
+    for c in get_collections():
+        if i > 0:
+            c.name = "".join(c.name.split(".")[:-1])
+        c.name = c.name + suffix
+        
+    for r in get_rigs():
+        if i > 0:
+            r.name = "".join(r.name.split(".")[:-1])
+        r.name = r.name + suffix
+        
+    for n in get_node_groups():
+        if i > 0:
+            n.name = "".join(n.name.split(".")[:-1])
+        n.name = n.name + suffix
 
-	filepath = bpy.data.filepath.replace(".blend", ".%d.blend"%i)
-	bpy.ops.wm.save_as_mainfile(filepath=filepath, copy=True)
+    filepath = bpy.data.filepath.replace(".blend", ".%d.blend"%i)
+    bpy.ops.wm.save_as_mainfile(filepath=filepath, copy=True)
