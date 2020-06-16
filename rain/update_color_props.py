947622aef576d259055ed371c620a9c55c524b77
Demeter Dzadik <demeterdzadik@gmail.com>
2020-06-16 21:22:39 +0200

    update update_color_props.py to work with latest cloudrig

--- rain/update_color_props.py
+++ rain/update_color_props.py
@@ -1,5 +1,4 @@
 import bpy
-from mets_tools.armature_nodes.driver import Driver
 
 # This script is meant for maintaining Rain's Viewport Display rig UI
 # Which lets us change viewport colors when the character is linked and proxied.
@@ -13,37 +12,37 @@ from mets_tools.armature_nodes.driver import Driver
 rig = bpy.context.object
 
 # Populate color properties
-rig.rig_colorproperties.clear()
+rig.cloud_colors.clear()
 mats_done = []
 for o in rig.children:
 	for ms in o.material_slots:
 		m = ms.material
 		if m in mats_done: continue
 		mats_done.append(m)
-		cp = rig.rig_colorproperties.add()
+		cp = rig.cloud_colors.add()
 		cp.name = m.name
-		cp.default = cp.color = m.diffuse_color[:-1]
+		cp.default = cp.current = m.diffuse_color[:-1]
 
 prop_dict = {}	# color property Name : Index map, which we will need for driver data paths, since we need to reference the color property by index then, but find it based on material name.
-for i, cp in enumerate(rig.rig_colorproperties):
+for i, cp in enumerate(rig.cloud_colors):
 	prop_dict[cp.name] = i
 
 for o in rig.children:
 	for ms in o.material_slots:
 		m = ms.material
 		
-		cpi = prop_dict[m.name]
+		color_index = prop_dict[m.name]
 		
 		# Add drivers to the first 3 values of the material color
 		for i in range(3):
-			d = Driver()
+			fcurve = m.driver_remove("diffuse_color", i)
+			fcurve = m.driver_add("diffuse_color", i)
+			d = fcurve.driver
 			d.type = 'SUM'
 
-			var = d.make_var("var")
+			var = d.variables.new()
 			var.type = 'SINGLE_PROP'
 
 			var_t = var.targets[0]
 			var_t.id = rig
-			var_t.data_path = 'rig_colorproperties[' + str(cpi) + '].color[' + str(i) + ']'
-			d.make_real(m, "diffuse_color", index=i)
-			
+			var_t.data_path = f"cloud_colors[{color_index}].current[{i}]"
