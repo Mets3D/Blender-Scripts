9cc1d79249110e82f8b1b4bb21732d2659675803
Mets3D <metssfm@gmail.com>
2020-10-27 16:30:08 +0100

    text

--- rain/rename_curves_after_bone_rename.py
+++ rain/rename_curves_after_bone_rename.py
@@ -1,47 +1,10 @@
 import bpy
 
 bone_dict = {
-	"Hook_Mouth_00" : "Hook_Mouth.L",
-	"Hook_L_Mouth_00" : "Hook_L_Mouth.L",
-	"Hook_R_Mouth_00" : "Hook_R_Mouth.L",
-	
-	"Hook_Mouth_02" : "Hook_Mouth.R",
-	"Hook_L_Mouth_02" : "Hook_R_Mouth.R",
-	"Hook_R_Mouth_02" : "Hook_L_Mouth.R",
-	
-	"Hook_L_Mouth_01" : "Hook_Top_Mouth.L",
-	"Hook_R_Mouth_01" : "Hook_Top_Mouth.R",
-	
-	"Hook_L_Mouth_03" : "Hook_Bot_Mouth.R",
-	"Hook_R_Mouth_03" : "Hook_Bot_Mouth.L",
-	
-	"Hook_Eye.L_00" : "Hook_Eye_00.L",
-	"Hook_Eye.L_01" : "Hook_Eye_01.L",
-	"Hook_Eye.L_02" : "Hook_Eye_02.L",
-	"Hook_Eye.L_03" : "Hook_Eye_03.L",
-	
-	"Hook_Eye.R_00" : "Hook_Eye_00.R",
-	"Hook_Eye.R_01" : "Hook_Eye_01.R",
-	"Hook_Eye.R_02" : "Hook_Eye_02.R",
-	"Hook_Eye.R_03" : "Hook_Eye_03.R",
-	
-	"Hook_L_Eye.L_00" : "Hook_L_Eye_00.L",
-	"Hook_R_Eye.L_00" : "Hook_R_Eye_00.L",
-	"Hook_L_Eye.L_01" : "Hook_L_Eye_01.L",
-	"Hook_R_Eye.L_01" : "Hook_R_Eye_01.L",
-	"Hook_L_Eye.L_02" : "Hook_L_Eye_02.L",
-	"Hook_R_Eye.L_02" : "Hook_R_Eye_02.L",
-	"Hook_L_Eye.L_03" : "Hook_L_Eye_03.L",
-	"Hook_R_Eye.L_03" : "Hook_R_Eye_03.L",
-	
-	"Hook_L_Eye.R_00" : "Hook_L_Eye_00.R",
-	"Hook_R_Eye.R_00" : "Hook_R_Eye_00.R",
-	"Hook_L_Eye.R_01" : "Hook_L_Eye_01.R",
-	"Hook_R_Eye.R_01" : "Hook_R_Eye_01.R",
-	"Hook_L_Eye.R_02" : "Hook_L_Eye_02.R",
-	"Hook_R_Eye.R_02" : "Hook_R_Eye_02.R",
-	"Hook_L_Eye.R_03" : "Hook_L_Eye_03.R",
-	"Hook_R_Eye.R_03" : "Hook_R_Eye_03.R",
+	"IK-MSTR-P-Wrist.L" : "IK-MSTR-Wrist.L",
+	"IK-MSTR-P-Wrist.R" : "IK-MSTR-Wrist.R",
+	"IK-MSTR-P-Foot.L" : "IK-MSTR-Foot.L",
+	"IK-MSTR-P-Foot.R" : "IK-MSTR-Foot.R",
 }
 
 for a in bpy.data.actions:
